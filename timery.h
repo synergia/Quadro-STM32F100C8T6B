88fc2a8ec2029edf4e264225b28d15ec35e3f9d2
Marcin Twardak <twardakm@gmail.com>
2014-05-21 16:58:11 +0200

    opis

--- timery.h
+++ timery.h
@@ -4,6 +4,14 @@
 #define TIMERY_H_
 #define T_PSC 80
 #define T_ARR 2
+/*
+ * 1kHz, 100 kroków
+ * 100 * ARR * PSC = 24 000 000 / 1 000
+ * ARR * PSC = 240
+ * ARR = 2 -> (0,1,2) -> 3
+ * PSC = 80
+ * 3 * 80 = 240
+ */
 #endif
 
 void inicjalizacja_SysTick();
