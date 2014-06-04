5823e6e45844ac96550377bfd8cff929b6565d93
Marcin Twardak <twardakm@gmail.com>
2014-06-04 23:05:29 +0200

    inicjalizacja i odczyt magnetometru

--- sensory.h
+++ sensory.h
@@ -4,6 +4,7 @@
 #define _SENSORY_H_
 
 #define ZYRO_ADR 0xD6
+#define MAGNET_ADR 0x3C
 
 #endif
 
@@ -11,7 +12,7 @@ void inicjalizacja_sensory();
 void inicjalizacja_zyroskop();
 void inicjalizacja_magnetometr();
 
-void odczyt_zyroskop();
-void odczyt_magnetometr();
+void odczyt_zyroskop(uint8_t *bufor);
+void odczyt_magnetometr(uint8_t *bufor);
 
 void odczyt_sensory();
